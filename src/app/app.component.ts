de1b9b07104a54ea496a2d747fd5a02a857c7fd2
Yademir Hernandez <yhernandezs@ufpso.edu.co>
2024-11-14 19:08:56 -0500

    environments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../environments/environment';
+import { ProductoService } from './services/producto.service';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, NgOptimizedImage],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title : string = 'FLDSMDFR';
+
+  prueba = environment.endPoint
+
+  private productService = inject(ProductoService);
+
+  producto$ = this.productService.getProducto(); 
 
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    endPoint: "esto es el endpoint de prueba"
+};
